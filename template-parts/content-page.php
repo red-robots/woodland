19696ea07a55af0c62253bd7d3042e1c10a73a0f
Fritz <fre.healy@gmail.com>
2017-04-14 14:02:52 -0400

    major styling page.php

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -9,33 +9,45 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php
-			the_content();
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'acstarter' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php
-			edit_post_link(
-				sprintf(
-					/* translators: %s: Name of current post */
-					esc_html__( 'Edit %s', 'acstarter' ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-		?>
-	</footer><!-- .entry-footer -->
+<article id="post-<?php the_ID(); ?>" <?php post_class("template-page"); ?>>
+	<?php $image = get_field("row_1_image");?>
+    <div class="row-1" <?php if($image): echo 'style="background-image: url('.$image['url'].');"'; endif;?>>
+        <div class="copy">
+			<?php $copy = get_field("row_1_copy");
+			if($copy):
+				echo $copy;
+			endif;?>
+        </div><!--.copy-->
+        <img src="<?php echo get_template_directory_uri()."/images/divot.png";?>" alt="">
+    </div><!--.row-1-->
+    <div class="row-2">
+        <h1><?php the_title();?></h1>
+        <?php if(get_the_content()):?>
+            <div class="copy">
+                <?php the_content();?>
+            </div><!--.copy-->
+        <?php endif;?>
+	    <?php $args = array(
+		    'post_type'=>'page',
+		    'post_parent'=>get_the_ID(),
+		    'posts_per_page'=>-1,
+		    'order'=>'ASC',
+		    'orderby'=>'menu_order'
+	    );
+	    $query = new WP_Query($args);
+	    if($query->have_posts()):?>
+            <div class="sub-pages">
+			    <?php while($query->have_posts()):$query->the_post();?>
+                    <div class="outer-wrapper">
+                        <div class="inner-wrapper">
+                            <a href="<?php echo get_the_permalink();?>">
+							    <?php the_title();?>
+                            </a>
+                        </div><!--.inner-wrapper-->
+                    </div><!--.outer-wrapper-->
+			    <?php endwhile;?>
+            </div><!--.row-2-->
+		    <?php wp_reset_postdata();?>
+	    <?php endif;?>
+    </div><!--.row-2-->
 </article><!-- #post-## -->
